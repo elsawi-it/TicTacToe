57408159597b55af857b8321b786406cb6214b4c
Ahmed <elsawi.it@gmail.com>
2020-08-16 07:51:31 +0300

    update App.java

--- Elsawi-TicTacToe/src/main/java/Ahmed/Elsawi/App.java
+++ Elsawi-TicTacToe/src/main/java/Ahmed/Elsawi/App.java
@@ -1,14 +1,15 @@
 package Ahmed.Elsawi;
 
+import java.io.IOException;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        TicTacToe t = new TicTacToe();
+        t.StartOrInfoOrExit();
+
     }
 }
