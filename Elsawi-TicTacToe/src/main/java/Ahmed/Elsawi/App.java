e324295105f5b9f1d77a887f065ac562c790dc14
Ahmed <elsawi.it@gmail.com>
2020-08-16 04:23:23 +0300

    Add App.java

---
+++ Elsawi-TicTacToe/src/main/java/Ahmed/Elsawi/App.java
@@ -0,0 +1,13 @@
+package Ahmed.Elsawi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
